6daa2c8d11edfd9c6f93b14953d8f8c20aaa53d5
Russel Zindoga <russellzindoga@gmail.com>
2024-10-29 19:34:14 +0200

    add django-models

---
+++ django-models/models/relationship_app/query_samples.py
@@ -0,0 +1,17 @@
+# relationship_app/query_samples.py
+from relationship_app.models import Book, Library, Librarian
+
+# Query all books by a specific author
+def books_by_author(author_name):
+    books = Book.objects.filter(author__name=author_name)
+    return books
+
+# List all books in a library
+def books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
+
+# Retrieve the librarian for a library
+def librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.librarian
